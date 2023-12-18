b90f34f905afbb9daa0519e88168c66eb14fa041
MersoAbreuDev <emerson.solutions@outlook.com>
2023-12-18 04:15:26 -0300

    insert manual

--- src/main/resources/db.sql
+++ src/main/resources/db.sql
@@ -1,5 +1,5 @@
-  INSERT INTO tb_usuario ( email, role, senha ,status,cpf)
- SELECT  'merso@gmail.com','ADMIN','$2a$12$W4ga0wBDtSMXGY0y41/PAuY8o0E.SGucCpddVRY6gvZhPxA16bpGi','ATIVO','123456'
+ INSERT INTO tb_usuario ( email, role, senha ,status,cpf)
+ SELECT  'merso@gmail.com','ADMIN','$2a$12$W4ga0wBDtSMXGY0y41/PAuY8o0E.SGucCpddVRY6gvZhPxA16bpGi','123456'
     WHERE NOT EXISTS (SELECT email FROM  tb_usuario WHERE id  = 2);
 
 
